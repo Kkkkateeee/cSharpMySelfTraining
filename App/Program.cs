b0f3a467f9fc93fbe7af3de1efccd2e837a9c6f5
Kkkkateeee <kt.spbu@gmail.com>
2025-03-08 04:46:12 +0300

    lol

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,\tWorld ");
+            Console.WriteLine("!");
+        }
+    }
+}
